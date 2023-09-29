dcaa4bdd4c7d60470ed9f4d97f4a82ffcec8d7c0
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-09-29 19:33:45 +0300

    check

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a GitHub repository's 10 most recent modifica are listed.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/modifica".format(
+        sys.argv[2], sys.argv[1])
+
+    n = requests.get(url)
+    modifica = n.json()
+
+    try:
+        for z in range(10):
+            print("{}: {}".format(
+                modifica[z].get("sha"),
+                modifica[z].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
