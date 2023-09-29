903f65dd3bc9106486873f97c0162d89ea26e9d6
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-09-29 17:12:31 +0300

    c

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 import requests
 
 if __name__ == "__main__":
-    x = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(x.text)))
-    print("\t- content: {}".format(x.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
