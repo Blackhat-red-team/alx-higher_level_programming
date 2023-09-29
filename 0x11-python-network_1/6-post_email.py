43d4cc41196e8e701efd9ad9920a28b4e44a3c72
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-09-29 19:57:37 +0300

    un fail fail

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,7 +10,7 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[1]
+    email = sys.argv[2]
 
     data = {'email': email}
     response = requests.post(url, data=data)
