32d57f9103a101169ec5769b43bc9c56c7187daf
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-09-29 17:00:53 +0300

    check

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,24 +9,13 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./6-post_email.py <URL> <email>")
-        sys.exit(1)
-
     url = sys.argv[1]
     email = sys.argv[2]
 
     data = {'email': email}
     response = requests.post(url, data=data)
 
-    email_printed = False  # متغير لتتبع ما إذا تم طباعة البريد الإلكتروني أم لا
-
     if response.status_code == 200:
-        if email in response.text:
-            print("Your email is:", email)
-            email_printed = True
-
-        print(response.text)
-
-    if not email_printed:
         print("Your email is:", email)
+
+    print(response.text)
