5a056b824e1aacc57112518b559e800aa76ee5ed
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-09-30 00:17:21 +0300

    check

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,19 +5,11 @@
 - of the X-Request-Id variable present in the respo's header.
 - stores the email in the variable email
 """
-import sys
 import requests
+import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
 
-    data = {'email': email}
-    response = requests.post(url, data=data)
-
-    if response.status_code == 200:
-        if email in response.text:
-            print("Your email is:", email)
-        print(response.text)
-    else:
-        print("Error: Email not found in the response")
+    data_info = {"email": sys.argv[2]}
+    requz = requests.post(sys.argv[1], data=data_info)
+    print(requz.text)
