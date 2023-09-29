68aa1e3102428d98bbd453ebf0975bbee2871b85
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-09-29 15:43:10 +0300

    fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,8 @@
 """fetches is https://intranet.hbtn.io/status."""
 import requests
 
-
 if __name__ == "__main__":
-    z = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(z.text)))
-    print("\t- content: {}".format(z.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
