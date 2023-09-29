107e7426e6d4520782d9a5594173b5babe1836d6
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-09-29 17:09:25 +0300

    fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -13,4 +13,3 @@ if __name__ == "__main__":
     request = urllib.request.Request(url_data)
     with urllib.request.urlopen(request) as response:
         print(dict(response.headers).get("X-Request-Id"))
-        
