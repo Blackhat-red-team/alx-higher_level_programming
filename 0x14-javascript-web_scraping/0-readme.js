494e62aa06afd0249734ee35fdbf3c1fc18642da
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-10-24 07:33:09 +0300

    test

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+try {
+  const content = fs.readFileSync(filePath, 'utf8');
+  console.log(content);
+} catch (error) {
+  console.error(error.message);
+}
